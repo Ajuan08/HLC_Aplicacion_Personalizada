76f1aff8bb19521fe30b7d5bf5b9d2bba01d4aab
Ajuan08 <ariofer0809@g.educaand.es>
2022-11-09 11:22:28 +0100

    Aplicacion_Personalizada_HLC

---
+++ Proyecto_Responsive_HLC/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Proyecto_Responsive_HLC',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
